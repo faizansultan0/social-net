41ed6e2ce9c6780a083d6577bc677f6e68ecd04f
Sultan Faizan Bahoo <faizansultan113@gmail.com>
2023-12-20 16:22:37 +0500

    Minor Changes

--- client/src/components/dashboard/postForm/postForm.jsx
+++ client/src/components/dashboard/postForm/postForm.jsx
@@ -3,60 +3,63 @@ import ReactQuill from "react-quill";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCamera } from "@fortawesome/free-solid-svg-icons";
 import "react-quill/dist/quill.snow.css";
-import './postForm.css';
+import "./postForm.css";
 
 const PostForm = ({
-  content,
-  setContent,
-  postSubmit,
-  handleImage,
-  uploading,
-  image,
+	content,
+	setContent,
+	postSubmit,
+	handleImage,
+	uploading,
+	image,
 }) => {
-  return (
-    <Form onSubmit={postSubmit} className="mb-4">
-      <div className="card">
-        <div className="card-body">
-          <Form.Group
-            controlId="exampleForm.ControlTextarea1"
-          >
-            <ReactQuill
-              theme="snow"
-              as="textarea"
-              value={content}
-              onChange={(e) => setContent(e)}
-              rows={3}
-              placeholder="Write Something..."
-            />
-          </Form.Group>
-        </div>
+	return (
+		<Form onSubmit={postSubmit} className="mb-4">
+			<div className="card">
+				<div className="card-body">
+					<Form.Group controlId="exampleForm.ControlTextarea1">
+						<ReactQuill
+							theme="snow"
+							as="textarea"
+							value={content}
+							onChange={(e) => setContent(e)}
+							rows={3}
+							placeholder="Write Something..."
+						/>
+					</Form.Group>
+				</div>
 
-        <div className="card-footer d-flex justify-content-between text-muted">
-          <button disabled={uploading || (!content && Object.keys(image).length === 0)} className="btn btn-sm btn-primary mt-1">
-            Post
-          </button>
-          <label>
-            {/* <FontAwesomeIcon icon={faCamera} className="mt-2" /> */}
-            {image && image.url ? (
-              <Image src={image.url} roundedCircle className="avatar" />
-            ) : uploading ? (
-              <Spinner animation="border" role="status" size="sm">
-                <span className="visually-hidden">Loading...</span>
-              </Spinner>
-            ) : (
-              <FontAwesomeIcon icon={faCamera} className="mt-2" />
-            )}
-            <input
-              onChange={handleImage}
-              type="file"
-              accept="images/*"
-              hidden
-            />
-          </label>
-        </div>
-      </div>
-    </Form>
-  );
+				<div className="card-footer d-flex justify-content-between text-muted">
+					<button
+						disabled={
+							uploading || (!content && Object.keys(image).length === 0)
+						}
+						className="btn btn-sm btn-primary mt-1"
+					>
+						Post
+					</button>
+					<label>
+						{/* <FontAwesomeIcon icon={faCamera} className="mt-2" /> */}
+						{image && image.url ? (
+							<Image src={image.url} roundedCircle className="avatar" />
+						) : uploading ? (
+							<Spinner animation="border" role="status" size="sm">
+								<span className="visually-hidden">Loading...</span>
+							</Spinner>
+						) : (
+							<FontAwesomeIcon icon={faCamera} className="mt-2" />
+						)}
+						<input
+							onChange={handleImage}
+							type="file"
+							accept="images/*"
+							hidden
+						/>
+					</label>
+				</div>
+			</div>
+		</Form>
+	);
 };
 
 export default PostForm;
